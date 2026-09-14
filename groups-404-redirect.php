d6e17e04860dd6cbaba1925eed0f3eb91ccc0188
George <gtsiokos@gmail.com>
2026-09-14 10:30:31 +0300

    updated dates

--- groups-404-redirect.php
+++ groups-404-redirect.php
@@ -2,7 +2,7 @@
 /**
  * groups-404-redirect.php
  *
- * Copyright (c) 2013-2025 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2013-2026 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
